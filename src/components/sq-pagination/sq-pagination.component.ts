779874f045b41fb5ad2b85e2d042c79229086653
Vinícius Marcili <vinipetter@gmail.com>
2023-08-28 19:08:04 -0300

    Fix pagination

--- src/components/sq-pagination/sq-pagination.component.ts
+++ src/components/sq-pagination/sq-pagination.component.ts
@@ -13,6 +13,7 @@ export class SqPaginationComponent implements OnInit, OnChanges, OnDestroy {
   @Input() currentPage = 1
   @Input() totalPages = 1
   @Input() showPages = 5
+  @Input() useQueryString = false
 
   @Output() pageChange: EventEmitter<number> = new EventEmitter<number>()
 
@@ -39,38 +40,66 @@ export class SqPaginationComponent implements OnInit, OnChanges, OnDestroy {
     if (changes['totalPages'] && changes['totalPages'].currentValue !== changes['totalPages'].previousValue) {
       this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1)
     }
+    if (changes['useQueryString'] && changes['useQueryString'].currentValue !== changes['useQueryString'].previousValue) {
+      if (this.useQueryString) {
+        this.mountQueryObservable()
+      } else {
+        this.destroyQueryObservable()
+      }
+    }
   }
 
   ngOnDestroy() {
+    this.destroyQueryObservable()
+  }
+
+  mountQueryObservable() {
+    this.routeObservable = this.route.queryParams.subscribe(search => {
+      const searchParams = new URLSearchParams(search)
+      const newPageQuery = parseInt(searchParams.get('page') || '1', 10)
+      if (newPageQuery !== this.currentPage) {
+        this.page = newPageQuery
+      }
+    })
+  }
+
+  destroyQueryObservable() {
     const searchParams = new URLSearchParams(window.location.search)
     searchParams.delete('page')
     this.router.navigate([], { relativeTo: this.route, queryParams: { page: null }, queryParamsHandling: 'merge' })
-    this.routeObservable.unsubscribe()
+    this.routeObservable?.unsubscribe()
   }
 
   canShow = useMemo((actualPage: number) => {
-    const half = Math.floor(this.showPages / 2)
-    return actualPage === this.currentPage || (actualPage >= this.currentPage - half && actualPage <= this.currentPage + half)
+    const half = this.calculateHalf()
+    return actualPage === this.page || (actualPage >= this.page - half && actualPage <= this.page + half)
   })
 
-  showDotMax = useMemo(() => {
-    const half = Math.floor(this.showPages / 2)
-    return this.currentPage + half < this.totalPages
+  showDotMax = useMemo((actualPage: number) => {
+    const half = this.calculateHalf()
+    return actualPage + half < this.totalPages
   })
 
-  showDotMin = useMemo(() => {
-    const half = Math.floor(this.showPages / 2)
-    return this.currentPage - half > 1
+  showDotMin = useMemo((actualPage: number) => {
+    const half = this.calculateHalf()
+    return actualPage - half > 1
   })
+
+  calculateHalf() {
+    return Math.floor(this.showPages / 2)
+  }
 
   handlePageChange(newPage: number) {
     if (newPage < 1 || newPage > this.totalPages) {
       return
     }
+    if (this.useQueryString) {
+      const searchParams = new URLSearchParams(window.location.search)
+      searchParams.set('page', newPage.toString())
+      this.router.navigate([], { relativeTo: this.route, queryParams: { page: newPage }, queryParamsHandling: 'merge' })
+    }
+
     this.page = newPage
-    const searchParams = new URLSearchParams(window.location.search)
-    searchParams.set('page', newPage.toString())
-    this.router.navigate([], { relativeTo: this.route, queryParams: { page: newPage }, queryParamsHandling: 'merge' })
     this.pageChange.emit(newPage)
   }
 }
