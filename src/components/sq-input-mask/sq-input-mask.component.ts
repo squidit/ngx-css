85cc0beed52f0c40e9761db14b5bf9517761bdfd
Icaro <icaroquadra@gmail.com>
2023-11-17 16:54:45 -0300

    :children_crossing: add min and max prperties to the sq-input-number

--- src/components/sq-input-mask/sq-input-mask.component.ts
+++ src/components/sq-input-mask/sq-input-mask.component.ts
@@ -5,12 +5,12 @@ import { SqInputComponent } from "../sq-input/sq-input.component"
 
 /**
  * Represents an input field with mask functionality that extends SqInputComponent.
- * 
+ *
  * This component uses ngx-mask library
  * @see {@link https://github.com/JsDaddy/ngx-mask}
- * 
+ *
  * This component extends the {@link SqInputComponent} and adds additional properties and behavior for handling money input.
- * 
+ *
  * @example
  * <sq-input-mask [(value)]='phone' [name]="'masked-input'" [id]="'masked-input'" [label]="'Masked Input'" [mask]="'(999) 999-9999'"></sq-input-mask>
  */
@@ -66,6 +66,16 @@ export class SqInputMaskComponent extends SqInputComponent {
   @Input() leadZero = false
 
   /**
+   * The character used as a placeholder for empty positions.
+   */
+  @Input() minValue?: number
+
+  /**
+   * The character used as a placeholder for empty positions.
+   */
+  @Input() maxValue?: number
+
+  /**
    * Reference to the native element.
    */
   override nativeElement: ElementRef
@@ -83,5 +93,55 @@ export class SqInputMaskComponent extends SqInputComponent {
   ) {
     super(validatorHelper, element, translate)
     this.nativeElement = element.nativeElement
+  }
+
+  /**
+   * Sets an error message.
+   *
+   * @param {string} key - The translation key for the error message.
+   * @param interpolateParams - Value to interpolate with translation.
+   */
+  override async setError(key: string, interpolateParams: Object = {}) {
+    if (this.useFormErrors && this.translate) {
+      this.error = await this.translate.instant(key, interpolateParams)
+    }
+  }
+
+  /**
+   * Handle input value changes.
+   * @param event - The input change event.
+   */
+  override async change(event: any) {
+    this.inFocus.emit(true)
+    this.value = event
+    clearTimeout(this.timeoutInput)
+    this.timeoutInput = setTimeout(() => {
+      this.valueChange.emit(event)
+    }, this.timeToChange)
+    this.validate()
+  }
+
+  /**
+   * Asynchronously validate the date input value.
+   * @param isBlur - Indicates if the input has lost focus.
+   */
+  override async validate(isBlur = false) {
+    const numericValue = Number(this.value); // Convert string to number
+
+    if (this.externalError) {
+      this.error = false
+    } else if (this.required && !this.value) {
+      this.valid.emit(false)
+      this.setError('forms.required')
+    } else if (this.maxValue && numericValue > this.maxValue) {
+      this.valid.emit(false)
+      this.setError('forms.maxValue', {minValue: this.maxValue})
+    } else if (this.minValue && numericValue < this.minValue) {
+      this.valid.emit(false)
+      this.setError('forms.minValue', {minValue: this.minValue})
+    } else {
+      this.valid.emit(true)
+      this.error = ''
+    }
   }
 }
