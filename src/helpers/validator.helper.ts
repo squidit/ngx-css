e71ba64c9805f38fe6982fdbcb9b3d2241f418d9
Caio Daló <caiodalo93@gmail.com>
2023-12-11 14:31:41 -0300

    fix: ajustado posição de regex de @

--- src/helpers/validator.helper.ts
+++ src/helpers/validator.helper.ts
@@ -62,8 +62,8 @@ export class ValidatorHelper {
       '^(https?:\\/\\/)' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\/\\@?[a-zA-Z0-9._%+-]*)*' +
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\/\\@?[a-zA-Z0-9._%+-]*)*' +
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
       '(\\#[-a-z\\d_]*)?$',
       'i'
