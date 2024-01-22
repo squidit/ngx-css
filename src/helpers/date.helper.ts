82b3bf43bb28e94d2d439c70d718b58c05f08af6
Daniel Paiva <dani_pcs@hotmail.com>
2024-01-22 14:33:40 -0300

    Melhoria na documentação do date helper.

--- src/helpers/date.helper.ts
+++ src/helpers/date.helper.ts
@@ -32,57 +32,61 @@ import { useMemo } from './memo.helper'
 export class DateHelper {
 
   /**
-   * TODO: Daniel colocar a descrição da constante
+   * Milliseconds per second, equal to 1.000 ms
    */
   readonly MS_PER_SECOND = 1000
 
   /**
-   * TODO: Daniel colocar a descrição da constante
+   * Milliseconds per minute, equal to 60.000 ms (60 seconds * 1000 milliseconds).
    */
   readonly MS_PER_MINUTE = 60 * this.MS_PER_SECOND
 
   /**
-   * TODO: Daniel colocar a descrição da constante
+   * Milliseconds per hour, equal to 3.600.000 ms (60 minutes * 60 seconds * 1000 milliseconds).
    */
   readonly MS_PER_HOUR = 60 * this.MS_PER_MINUTE
 
-  /**
-   * TODO: Daniel colocar a descrição da constante
+  /** 
+   * Milliseconds per day, equal to 86,400,000 ms (24 hours * 60 minutes * 60 seconds * 1000 milliseconds).
    */
   readonly MS_PER_DAY = 24 * this.MS_PER_HOUR
 
   /**
-   * TODO: Daniel colocar a descrição da constante
+   * Milliseconds per week, equal to 604.800.000 ms (7 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds).
    */
   readonly MS_PER_WEEK = 7 * this.MS_PER_DAY
 
   /**
-   * Uses Angular's formatDate to format the date using the provided format and timezone
-   * @param date
-   * @param format Defaults to yyyy-MM-ddTHH:mm:ssZZZZZ
-   * @param timezone Optional timezone
-   * @see {@link https://angular.io/api/common/formatDate}
-   * @returns String with the formatted date
+   * Formats the given date using Angular's formatDate function, allowing customization of the output format and timezone.
+   * 
+   * @param date - The Date object to be formatted.
+   * @param format - (Optional) The format string for the output. Defaults to 'yyyy-MM-ddTHH:mm:ssZZZZZ'.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @param locale - (Optional) A string representing the locale. Defaults to 'en-US'.
+   * @see {@link https://angular.io/api/common/formatDate} - Angular formatDate documentation for additional format options.
+   * @returns A string containing the formatted date.
    */
   format(date: Date, format = 'yyyy-MM-ddTHH:mm:ssZZZZZ', timezone?: string, locale = 'en-US'): string {
     return formatDate(date, format, locale, timezone)
   }
 
   /**
-   * Returns the Date offset by timezone
-   * @param date 
-   * @param timezone 
-   * @returns Date offset by timezone
+   * Returns the Date offset by the provided timezone.
+   * 
+   * @param date - The input Date object.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns Date offset by the specified timezone.
    */
   timezoneDate(date: Date, timezone?: string): Date {
     return new Date(date.toLocaleString('en-US', { timeZone: timezone }))
   }
 
   /**
-   * TODO: Daniel colocar a descrição da função
-   * @param date 
-   * @param timezone 
-   * @returns TODO: Daniel colocar o retorno da função
+   * Calculates and returns the first moment of the day for a given date, considering the provided timezone.
+   * 
+   * @param date - The reference date for which to determine the start of the day.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns A Date object representing the first moment of the day at 00:00:00.
    */
   startOfDay(date: Date, timezone?: string): Date {
     const tzDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()).toLocaleString('en-US', {timeZone: timezone})
@@ -90,10 +94,11 @@ export class DateHelper {
   }
 
   /**
-   * TODO: Daniel colocar a descrição da função
-   * @param date 
-   * @param timezone 
-   * @returns TODO: Daniel colocar o retorno da função
+   * Calculates and returns the last moment of the day for a given date, considering the provided timezone.
+   * 
+   * @param date - The reference date for which to determine the end of the day.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns A Date object representing the last moment of the day at 23:59:59.
    */
   endOfDay(date: Date, timezone?: string): Date {
     const startOfDay = this.startOfDay(date, timezone)
@@ -101,10 +106,11 @@ export class DateHelper {
   }
 
   /**
-   * TODO: Daniel colocar a descrição da função
-   * @param date 
-   * @param timezone 
-   * @returns TODO: Daniel colocar o retorno da função
+   * Calculates and returns the first moment of the month for a given date, considering the provided timezone.
+   * 
+   * @param date - The reference date for which to determine the start of the month.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns A Date object representing the first moment of the month at 00:00:00.
    */
   startOfMonth(date: Date, timezone?: string): Date {
     const localeDate = this.startOfDay(date, timezone)
@@ -112,10 +118,11 @@ export class DateHelper {
   }
 
   /**
-   * TODO: Daniel colocar a descrição da função
-   * @param date 
-   * @param timezone 
-   * @returns TODO: Daniel colocar o retorno da função
+   * Calculates and returns the last moment of the month for a given date, considering the provided timezone.
+   * 
+   * @param date - The reference date for which to determine the end of the month.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns A Date object representing the last moment of the month at 23:59:59.
    */
   endOfMonth(date: Date, timezone?: string): Date {
     const localeDate = this.startOfDay(date, timezone)
@@ -124,13 +131,14 @@ export class DateHelper {
   }
 
   /**
-    *  Returns the difference between two dates in the specified unit
-    *  @param unit : 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks'
-    *  @param firstDate: Date
-    *  @param secondDate: Date
-    *  @param timezone: string
-    *  @returns number
-  */
+   * Returns the difference between two dates in the specified unit.
+   * 
+   * @param unit - The unit of difference: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks'.
+   * @param firstDate - The first Date object.
+   * @param secondDate - The second Date object.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @returns The difference between the two dates in the specified unit.
+   */
   diffDate(unit: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks', firstDate: Date, secondDate: Date, timezone?: string): number {
     let unitTime: number
     switch(unit){
@@ -158,30 +166,31 @@ export class DateHelper {
   }
 
   /**
-    * Returns an array with all days of the week translated to the locale provided
-    * 
-    * @param locale: string - Locale
-    * @returns string[]
+   * Returns an array with the names of all days of the week, translated to the specified locale.
+   * 
+   * @param locale - A string representing the locale. Defaults to 'en-US'.
+   * @returns An array containing the names of all days of the week.
    */
   weekdays = useMemo((locale = 'en-US'): Array<string> => {
-    const date = new Date()
+    const currentDate = new Date()
     const daysOfWeek = []
 
     for (let i = 0; i < 7; i++) {
-      const day = new Date(date)
-      day.setDate(date.getDate() - date.getDay() + i)
+      const day = new Date(currentDate)
+      day.setDate(currentDate.getDate() - currentDate.getDay() + i)
       daysOfWeek.push(day.toLocaleDateString(locale, { weekday: 'long' }))
     }
     return daysOfWeek
   })
 
   /**
-   * Returns a date with the selected week day
-   * @param day  From 0 to 6, where 0 is Sunday and 6 is Saturday. It can be positive for next weeks or negative for previous weeks
-   * @param date 
-   * @param timezone 
-   * @returns Date with the day of the week
-   */
+    * Returns a Date with the specified day of the week.
+    * 
+    * @param day - From 0 to 6, where 0 is Sunday and 6 is Saturday. It can be positive for future weeks or negative for past weeks.
+    * @param date - (Optional) The reference Date object. Defaults to the current date.
+    * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+    * @returns Date object representing the specified day of the week.
+    */
   weekday(day: number, date = new Date(), timezone?: string): Date {
     date = this.timezoneDate(date, timezone)
     const currentDayOfWeek = date.getDay()
@@ -191,12 +200,12 @@ export class DateHelper {
   }
 
   /**
-    * Returns a translated string with the date's day of the week.
-    * 
-    * @param date {Date}
-    * @param timezone {String}
-    * @param locale: string - Locale
-    * @returns string[]
+   * Returns the name of the day of the week for a given date, translated to the specified locale.
+   * 
+   * @param date - The input date for which to determine the day of the week.
+   * @param timezone - (Optional) The timezone to consider. If not provided, the local timezone is used.
+   * @param locale - (Optional) A string representing the locale. Defaults to 'en-US'.
+   * @returns A string representing the full name of the day of the week.
    */
   dayOfTheWeek(date: Date, timezone?: string, locale = 'en-US'){
     const tzDate = this.timezoneDate(new Date(date), timezone)
@@ -205,10 +214,11 @@ export class DateHelper {
 
   /**
    * Adds the specified units to the date provided
-   * @param value The amount of units to add
-   * @param unit The unit to add, it can be 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months'
-   * @param date The date to add the units
-   * @param timezone The timezone to add the units
+   * 
+   * @param value - The amount of units to add
+   * @param unit - The unit to add, it can be 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months'
+   * @param date - The date to add the units
+   * @param timezone - The timezone to add the units
    * @returns The new date with the units added
    */
   addDate(value: number, unit: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months', date: Date, timezone?: string): Date {
@@ -241,9 +251,10 @@ export class DateHelper {
   }
 
   /**
-   * Verifies if the date is valid
-   * @param date 
-   * @returns true if the date is valid, false otherwise
+   * Checks if the provided date object is a valid date.
+   * 
+   * @param date - The date object to be checked for validity.
+   * @returns `true` if the date is valid; `false` otherwise.
    */
   isDateValid(date: Date): boolean {
     return date instanceof Date && !isNaN(date.getTime())
