94cf452a5d7c412cd257af6e9f769a7c3f1f40ba
Levy Henrique Alves Nunes <56304278+levyhanunes@users.noreply.github.com>
2024-08-22 09:37:34 -0300

    Revert "remove html tags pipe"

--- src/main.module.ts
+++ src/main.module.ts
@@ -30,7 +30,6 @@ import { SqSelectSearchComponent } from './components/sq-select-search/sq-select
 import { SqClickOutsideDirective } from './directives/sq-click-outside/sq-click-outside.directive'
 import { SearchPipe } from './pipes/search/search.pipe'
 import { BirthdatePipe } from './pipes/birthdate/birthdate.pipe'
-import { RemoveHtmlTagsPipe } from './pipes/remove-html-tags/remove-html-tags.pipe'
 import { SearchFromAlternativeArrayPipe } from './pipes/search-from-alternative-array/search-from-alternative-array.pipe'
 import { SearchValidValuesPipe } from './pipes/search-valid-values/search-valid-values.pipe'
 import { SqSelectMultiTagsComponent } from './components/sq-select-multi-tags/sq-select-multi-tags.component'
@@ -83,8 +82,7 @@ const components: (Type<any> | any)[] = [
   SqInputMaskComponent,
   SqInputMoneyComponent,
   SqInputNumberComponent,
-  TranslateInternalPipe,
-  RemoveHtmlTagsPipe
+  TranslateInternalPipe
 ]
 
 /**
@@ -110,4 +108,4 @@ const components: (Type<any> | any)[] = [
     NgxMaskPipe
   ]
 })
-export class SquidCSSModule { }
+export class SquidCSSModule {}
